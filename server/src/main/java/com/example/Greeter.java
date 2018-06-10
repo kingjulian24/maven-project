2c5e13a5b4715047aaf2e92213acdc288a2d0e32
Julian Brown <julian.brown@cogecopeer1.com>
2018-06-10 15:02:44 -0400

    Checkstyle fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,7 +15,7 @@ public class Greeter {
   /**
    * Greeting comment.
    * @param someone Your name.
-   * @return
+   * @return Greeting.
    */
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
